72d93f0f1bb5da04f7859d97c7e1c3917f9b3317
Dodo9531 <oge2782@mail.ru>
2023-07-06 21:56:34 +0300

    swagger param fix

--- src/main/java/com/example/tour/controller/UserController.java
+++ src/main/java/com/example/tour/controller/UserController.java
@@ -3,8 +3,8 @@ package com.example.tour.controller;
 import com.example.tour.model.UserModel;
 import com.example.tour.service.UserServiceImpl;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.hibernate.annotations.Parameter;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -22,36 +22,37 @@ public class UserController {
     }
     @Operation(summary = "Get all users", description = "Returns all users", method = "GET")
     @GetMapping
-    public Page<UserModel> list(@RequestParam(defaultValue = "0") Integer page,
-                                @RequestParam(defaultValue = "10") Integer size,
-                                @RequestParam(defaultValue = "id") String sort)
+    public Page<UserModel> list(
+            @Parameter(name = "page", description = "Page", required = true) @RequestParam(defaultValue = "0") Integer page,
+            @Parameter(name = "size", description = "Page size", required = true) @RequestParam(defaultValue = "10") Integer size,
+            @Parameter(name = "sort", description = "Page sort", required = true) @RequestParam(defaultValue = "id") String sort)
     {
         Pageable pageable = PageRequest.of(page,size, Sort.by(sort));
         return service.getall(pageable);
     }
     @Operation(summary = "Create user", description = "Create user with given parameters")
     @PostMapping("/create")
-    public UserModel create(@RequestBody UserModel userModel)
+    public UserModel create(  @Parameter(name = "user", description = "user object", required = true) @RequestBody UserModel userModel)
     {
         return service.create(userModel);
     }
     @Operation(summary = "Get member by ID", description = "Returns a single member by their ID")
     @GetMapping("/{id}")
-    public UserModel getbyid(@PathVariable Long id)
+    public UserModel getbyid(  @Parameter(name = "id", description = "user id", required = true) @PathVariable Long id)
     {
         if(service.getbyid(id).isPresent()) return service.getbyid(id).get();
         else return null;
     }
     @Operation(summary = "Update user by ID", description = "Update a single user by their ID")
     @PutMapping("/{id}")
-    public UserModel update(@PathVariable Long id,@RequestBody UserModel userModel)
+    public UserModel update(@Parameter(name = "id", description = "user id", required = true)@PathVariable Long id,@Parameter(name = "user", description = "user object", required = true)@RequestBody UserModel userModel)
     {
             if(service.getbyid(id).isPresent()) return service.redact(id,userModel);
             else return null;
     }
     @Operation(summary = "Delete user by ID", description = "Delete a single user by their ID")
     @DeleteMapping("/{id}")
-    public void delete (@PathVariable Long id)
+    public void delete (@Parameter(name = "id", description = "user id", required = true)@PathVariable Long id)
     {
         if(service.getbyid(id).isPresent())  service.delete(id);
     }
